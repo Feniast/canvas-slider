7259861d338df9dee544b83f9faa6b82c3cda2b9
tang-chen-yang <tang-chen-yang@hpe.com>
2018-11-02 18:44:49 +0800

    [WIP]

--- src/CanvasSlider.js
+++ src/CanvasSlider.js
@@ -7,16 +7,58 @@ const resizeDetector = ElementResizeDetector({
   strategy: 'scroll'
 });
 
-/**
- * image item structure
-```
-{ 
-  url: '', 
-  key: '' 
-}
-```
- */
 class CanvasSlider {
+  static defaultPreset = {
+    minScale: 0.5,
+    maxScale = 1,
+    movingScale: 0.5,
+    minClip = 0,
+    maxClip = .15,
+    minAlpha = 0,
+    maxAlpha = 1,
+    minPan = -.05,
+    maxPan = 1.05,
+    maxImageStretch: 500,
+    alphaSpeed: 0.25,
+    scaleSpeed: 0.1,
+    clipSpeed: 0.05,
+    panSpeed: 0.07,
+    dragSpeedFactor: 0.06,
+    horizontalSpacerFactor: 0.6,
+    scrollingScaleOffset: 1
+  }
+
+  static presets = [
+    {
+      predicate: ({ width, height }) => {
+        return width < 768;
+      },
+      settings: {
+        minScale: 0.8,
+        movingScale: 0.8,
+        maxImageStretch: 200,
+        scaleSpeed: 0.2,
+        clipSpeed: 0.2,
+        panSpeed: 0.15,
+        dragSpeedFactor: 0.15
+      }
+    },
+    {
+      predicate: ({ width, height }) => {
+        return width >= 768;
+      },
+      settings: {
+        minScale: 0.5,
+        movingScale: 0.5,
+        maxImageStretch: 500,
+        scaleSpeed: 0.1,
+        clipSpeed: 0.05,
+        panSpeed: 0.07,
+        dragSpeedFactor: 0.06
+      }
+    }
+  ]
+
   constructor(opts) {
     if (!opts.el) throw new Error('the root element must be provided');
     this.el = opts.el;
@@ -106,7 +148,7 @@ class CanvasSlider {
         image
       };
     });
-    this.loadImages = loadedImages;
+    this.loadedImages = loadedImages;
     this.imagesLoaded = true;
   }
 
@@ -128,6 +170,32 @@ class CanvasSlider {
     // display size
     canvas.style.width = `${this.width}px`;
     canvas.style.height = `${this.height}px`;
+  }
+
+  setImageSizes() {
+    const remainingSpacing = this.isRetina ? 200 : 126;
+    this.loadedImages.forEach(image => {
+      const xGap = this.width - image.width;
+      const yGap = this.height - image.height;
+      const ratio = image.ratio;
+      if (xGap < yGap) {
+        const w = this.width - remainingSpacing;
+        image.xSize = w;
+        image.ySize = w / ratio;
+      } else {
+        const h = this.height - remainingSpacing;
+        image.xSize = h * ratio;
+        image.ySize = h;
+      }
+    });
+  }
+
+  setImageScales() {
+    this.loadedImages.forEach((image, idx) => {
+      if (this.activeItem === idx) {
+        image.xScale = 
+      }
+    })
   }
 
   nextItem() {
